adc6669fc4759828184a6c3edc9b8d82b1874018
Morning Star <vsvsq8@hotmail.com>
2018-05-23 00:09:07 +0300

    Added the vec3 support. Maybe ofstream should be used insted of freopen?..

--- main.cpp
+++ main.cpp
@@ -1,21 +1,30 @@
 #include <iostream>
+#include <cstdio>
+
+#include "vec3.hpp"
+
 
 int main() {
+  freopen("output.ppm", "w", stdout);
+
   int nx = 200;
   int ny = 100;
   std::cout << "P3\n" << nx << " " << ny << "\n255\n";
 
   for (int j = ny-1; j >= 0; j--){
     for (int i =0; i< nx; i++){
-      float r = float(i) / float(nx);
-      float g = float(j) / float(ny);
-      float b = 0.2;
+      /// float r = float(i) / float(nx);
+      /// float g = float(j) / float(ny);
+      /// float b = 0.2;
+      vec3 col(float(i) / float(nx), float(j) / float(ny), 0.2);
 
-      int ir = int(255.99*r);
-      int ig = int(255.99*g);
-      int ib = int(255.99*b);
+      int ir = int(255.99*col[0]);
+      int ig = int(255.99*col[1]);
+      int ib = int(255.99*col[2]);
 
       std::cout << ir << " " << ig << " " << ib << "\n";
     }
   }
+
+  return 0;
 }
