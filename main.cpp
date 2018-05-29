502693dc29893e8cb03ae0cbff13183bb69686f4
Morning Star <vsvsq8@hotmail.com>
2018-05-29 05:58:48 +0300

    Chapter 11: Defocus Blur.

--- main.cpp
+++ main.cpp
@@ -52,7 +52,12 @@ int main() {
 
   hitable *world = new hitable_list(list, 5);
 
-  camera cam(vec3(-2,2,1), vec3(0,0,-1), vec3(0,1,0), 20, float(nx)/float(ny));
+  vec3 lookfrom(3,3,2);
+  vec3 lookat(0,0,-1);
+  float dist_to_focus = (lookfrom-lookat).length();
+  float apture = 2.0;
+
+  camera cam(lookfrom, lookat, vec3(0,1,0), 20, float(nx)/float(ny), apture, dist_to_focus);
 
   /// float R = cos(M_PI/4);
   /// list[0] = new sphere(vec3(-R, 0, -1), R, new lambertian(vec3(0,0,1)));
