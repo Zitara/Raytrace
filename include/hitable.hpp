168d96d1c7ff32a1b60305cb7c6ad3af4ca843a8
Zitara <vsvsq8@hotmail.com>
2018-08-22 05:15:08 +0300

    amended?

--- include/hitable.hpp
+++ include/hitable.hpp
@@ -15,6 +15,7 @@ struct hit_record{
 class hitable {
   public:
     virtual bool hit(const ray &r, float t_min, float t_max, hit_record &rec) const = 0;
+//    virtual ~hitable() = 0;
 };
 
 #endif
--- include/progressBar.hpp
+++ include/progressBar.hpp
@@ -25,15 +25,15 @@ public:
 
     void display() const
     {
-        float progress = ticks / total_ticks;
-        unsigned int pos = (bar_width * progress);
+        float progress = (float) ticks / total_ticks;
+        int pos = (bar_width * progress);
 
         std::chrono::steady_clock::time_point now = std::chrono::steady_clock::now();
         auto time_elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(now-start_time).count();
 
         std::cout << "[";
 
-        for (unsigned int i = 0; i < bar_width; ++i) {
+        for (int i = 0; i < bar_width; ++i) {
             if (i < pos) std::cout << complete_char;
             else if (i == pos) std::cout << ">";
             else std::cout << incomplete_char;
